88f46fb528554c4bb648455e8aa3a07e7bf86067
Gil Rodarte <yepes@Yepes.local>
2017-12-09 21:25:52 -0600

    CollectionView usando API

---
+++ CollectionViewImagenes/CustomCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCollectionViewCell.swift
+//  CollectionViewImagenes
+//
+//  Created by Gil Rodarte on 09/12/17.
+//  Copyright © 2017 Gil Rodarte. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+}
---
+++ CollectionViewImagenes/ViewController.swift
@@ -0,0 +1,104 @@
+//
+//  ViewController.swift
+//  CollectionViewImagenes
+//
+//  Created by Gil Rodarte on 09/12/17.
+//  Copyright © 2017 Gil Rodarte. All rights reserved.
+//
+
+import UIKit
+
+struct Hero: Decodable {
+    let localized_name: String
+    let img: String
+}
+
+class ViewController: UIViewController, UICollectionViewDataSource {
+    
+    var heroes = [Hero]()
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.dataSource = self
+        
+        let url = URL(string: "https://api.opendota.com/api/heroStats")
+        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+            if error == nil {
+                do {
+                    self.heroes = try JSONDecoder().decode([Hero].self, from: data!)
+                }catch{
+                    print("Parse ERROR!")
+                }
+                
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                }
+            }
+            
+        }.resume()
+       
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return heroes.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "customCell", for: indexPath) as! CustomCollectionViewCell
+        
+        cell.nameLbl.text = heroes[indexPath.row].localized_name.capitalized
+        
+        cell.imageView.contentMode = .scaleToFill
+        
+        let defaultLink = "https://api.opendota.com"
+        let completeLink = defaultLink + heroes[indexPath.row].img
+        
+        cell.imageView.downloadedFrom(link: completeLink)
+        cell.imageView.clipsToBounds = true
+        cell.imageView.layer.cornerRadius = cell.imageView.frame.height / 2
+        cell.imageView.contentMode = .scaleToFill
+        
+        return cell
+    }
+    
+}
+
+extension UIImageView {
+    func downloadedFrom(url: URL, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() {
+                self.image = image
+            }
+            }.resume()
+    }
+    func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloadedFrom(url: url, contentMode: mode)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
